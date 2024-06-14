09982bf3781bde56547ac154b8454e713d24d4e5
Gerson Arbigaus <gerson87@gmail.com>
2024-06-14 19:57:35 -0300

    Configure `neo-tree` to show hidden files

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -3,12 +3,20 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-    vim.keymap.set('n', '<leader>n', ':Neotree close<CR>', {})
-  end
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
+    vim.keymap.set("n", "<leader>n", ":Neotree close<CR>", {})
+  end,
 }
-
