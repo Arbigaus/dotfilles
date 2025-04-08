b35d3fd3842c8a4efa804ba0adc20c103f947e31
Arbigaus <gerson87@gmail.com>
2025-04-08 20:18:00 -0300

    New nvim configurations

--- .config/nvim/lua/config/plugins/oil.lua
+++ .config/nvim/lua/config/plugins/oil.lua
@@ -9,5 +9,25 @@ return {
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
+    config = function()
+      local oil = require('oil')
+      oil.setup({
+        view_options = {
+          show_hidden = true
+        },
+        git = {
+          -- Return true to automatically git add/mv/rm files
+          add = function(path)
+            return false
+          end,
+          mv = function(src_path, dest_path)
+            return false
+          end,
+          rm = function(path)
+            return false
+          end,
+        },
+      })
+    end
   }
 }
